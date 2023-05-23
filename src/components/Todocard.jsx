4258d416bf9a04cf4838f75e18304bafd15f5db2
Haikal15 <haikalfajari156@gmail.com>
2023-05-24 00:30:25 +0700

    modify Todocard

--- src/components/Todocard.jsx
+++ src/components/Todocard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { GiCheckMark } from "react-icons/gi";
 import { RiEdit2Fill } from "react-icons/ri";
 import { FaTrash } from "react-icons/fa";
@@ -9,9 +9,12 @@ const TodoCard = ({ todo }) => {
   const dispatch = useDispatch();
   const [editing, setEditing] = useState(false);
   const [updatedContent, setUpdatedContent] = useState(todo.content);
+  const inputRef = useRef(null);
 
   const handleToggle = (id) => {
-    dispatch(actions.toggleTodo({ id }));
+    if (!editing) {
+      dispatch(actions.toggleTodo({ id }));
+    }
   };
 
   const handleRemove = (id) => {
@@ -27,34 +30,48 @@ const TodoCard = ({ todo }) => {
     setEditing(false);
   };
 
+  const handleKeyDown = (e, id) => {
+    if (e.key === "Enter") {
+      handleSave(id);
+    }
+  };
+
+  useEffect(() => {
+    if (editing) {
+      inputRef.current.focus();
+    }
+  }, [editing]);
+
   return (
     <div className="flex justify-between p-2 bg-slate-300 rounded mb-4 items-center">
       {editing ? (
-        <input type="text" value={updatedContent} onChange={(e) => setUpdatedContent(e.target.value)} className="border border-gray-300 rounded p-1" />
+        <input type="text" value={updatedContent} onChange={(e) => setUpdatedContent(e.target.value)} className="border border-gray-300 rounded p-1" ref={inputRef} onKeyDown={(e) => handleKeyDown(e, todo.id)} />
       ) : (
         <p className="font-semibold" style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
           {todo.content}
         </p>
       )}
-      <div className="flex gap-1 items-center">
-        <input type="checkbox" name="completed" checked={todo.completed} onChange={() => handleToggle(todo.id)} />
-        <label className="font-medium" htmlFor="completed">
-          Completed
-        </label>
-        <div></div>
-        {editing ? (
-          <button className="btn" onClick={() => handleSave(todo.id)}>
-            <GiCheckMark />
+      {!todo.completed && (
+        <div className="flex gap-1 items-center">
+          <input type="checkbox" name="completed" checked={todo.completed} onChange={() => handleToggle(todo.id)} />
+          <label className="font-semibold" htmlFor="completed">
+            Completed
+          </label>
+          <div></div>
+          {editing ? (
+            <button className="btn" onClick={() => handleSave(todo.id)}>
+              <GiCheckMark />
+            </button>
+          ) : (
+            <button className="btn" onClick={handleEdit}>
+              <RiEdit2Fill />
+            </button>
+          )}
+          <button className="btn" onClick={() => handleRemove(todo.id)}>
+            <FaTrash />
           </button>
-        ) : (
-          <button className="btn" onClick={handleEdit}>
-            <RiEdit2Fill />
-          </button>
-        )}
-        <button className="btn" onClick={() => handleRemove(todo.id)}>
-          <FaTrash />
-        </button>
-      </div>
+        </div>
+      )}
     </div>
   );
 };
